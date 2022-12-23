9a6e341e548fd88c3c02a082bff3cdea32fb1db9
chethtrayen <chethtrayen1991@gmail.com>
2022-12-23 08:50:47 -0500

    postman

--- app/client/src/components/sections/Results.js
+++ app/client/src/components/sections/Results.js
@@ -1,3 +1,22 @@
+import { useQuery, gql } from "@apollo/client";
+
+const GET_RESULT = gql`
+  query poll($id: String!) {
+    poll(id: $id) {
+      _id
+      closed
+      label
+      userVote
+      options {
+        _id
+        label
+        result
+      }
+      winner
+    }
+  }
+`;
+
 export const Results = ({}) => {
   return <>results</>;
 };
